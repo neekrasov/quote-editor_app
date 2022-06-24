c8b4c826e1fd13d30b4a9e04aab9817b46a7edde
neekrasov <neekrasov@gmail.com>
2022-06-25 01:38:43 +0300

    Added config class

---
+++ src/main/java/com/app/Configs.java
@@ -0,0 +1,9 @@
+package com.app;
+
+public class Configs {
+    protected static String dbHost = "std-mysql";
+    protected static String dbPort = "3306";
+    protected static String dbName = "std_1969_quote_editor";
+    protected static String dbUser = "std_1969_quote_editor";
+    protected static String dbPassword = "123456789";
+}
